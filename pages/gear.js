1bd38df193996f609d78b8b341e491a76e5e273b
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2022-03-13 19:35:47 +0000

    transparency and spacing issues on mobile addressed

---
+++ pages/gear.js
@@ -0,0 +1,238 @@
+import styles from '../styles/Gear.module.css';
+
+export default function Gear() {
+
+  return (
+    <div className={styles.container}>
+
+Guitars
+
+ 
+
+Fender Jaguar HH with Bare Knuckle Pickups
+
+Fender Mustang with Seymour Duncan Hot Rail
+
+Fender Japanese Strat with Bare Knuckle Pickups
+
+Squire Bass VI
+
+Tokai Telecaster
+
+Squire Statocaster with Seymour Duncan Invader
+
+Fender Jazz bass w/ Seymour Duncan Quarter Pounders
+
+Martin 000-15M Acoustic
+
+Amps
+
+​
+
+Vox AC30 guitar amp combo
+
+Marshall JCM800 guitar amp head
+
+Marshall JCM900 guitar amp head
+
+Orange Rockerverb 50 guitar amp head
+
+Peavey 6505 guitar amp head
+
+Peavey 5150 guitar amp head
+
+Carlsbro 50 top guitar amp head
+Fender Deluxe guitar amp combo
+
+MESA Boogie 4x12 guitar cab
+Fender Tonemaster 4x12 cab
+
+Marshall JCM900 4x12 guitar cab
+
+Ampeg PF500 bass head
+Custom bass cab with Eminence speakers
+
+Pedals
+
+​
+
+Strymon Big Sky reverb
+
+Stymon Timeline
+Walrus Slo reverb
+
+Walrus Descent reverb
+JHS Big Cheese fuzz
+
+Empress Tape Delay
+Fulltone OCD distortion
+
+J-Rocket Archer overdrive
+
+Fulltone Plimsole distortion
+Electro Harmonix Russian Big Muff fuzz 
+Electro Harmonix USA Big Muff fuzz
+Electro Harmonix Small Clone chorus
+
+Electro Harmonix Small Stone phaser 
+
+Electro Harmonix POG2 octave generator
+
+Electro Harmonix Super Ego + Synth Pedal 
+
+TC Electronics Hall of Fame reverb
+
+TC Electronics Triple delay
+TC Electronic Polytune Mini
+TC Electronic Sub n Up octave pedal
+ZVex Fuzz Factory fuzz
+MXR Phase100
+
+MXR micro flanger
+
+MXR Dyna Comp compressor
+Ibanez TubeScreamer
+
+Keeley Super Phat Mod distortion
+
+Keeley Compressor Plus 
+
+Blackout Effectors Blunderbuss fuzz
+
+Pro-co RAT distortion
+
+Made By Mike Green Muff fuzz
+
+Boss TR-2 tremelo pedal
+
+Boss DD4 delay
+Boss CH1 chorus
+Boss CE5 chorus
+
+Boss CS3 compressor
+
+Boss CEB3 bass chorus
+Boss SD1 overdrive
+
+Boss ODB-3 Bass overdrive
+
+Digitech Synth Wah
+
+Lots of DIY handmade pedals
+
+...many more pedals
+
+​
+
+Drum Stuff
+
+​
+
+Mapex Black Panther 14x6 maple snare
+
+Mapex Black Panther Chris Adler 12x6 maple snare
+
+Ludwig Black Beauty 14x6 brass snare
+
+Vintage Premier Steel 14 Snare
+
+Vintage 80s Yamaha Stage Classic Mahogony drum kit
+
+Tama Superstar Birch drum kit
+Dream Bliss Crash and Ride Cymbals (for hire)
+Zildjian A Custom New Beat Hi-Hats (For Hire)
+
+​
+
+Microphones
+
+
+AKG C414 XLS x 2
+Coles 4038 x2
+
+Audio Technica AT4033 x 2
+JZ V67 x2
+
+Cascade Fathead w/ Lundahl transformer x2
+
+Warm Audio WA87
+SE Electronics R1
+
+Aston Stealth
+
+Shure Sm7B
+
+Beyerdynamic M201
+
+Sennheiser MD421 x2
+
+Rode NT5 x2
+
+Rode NTK
+
+Shure Beta 57
+
+Shure SM58 x2
+
+Shure SM57 x 3
+Shure Beta52
+
+Audix D6
+
+AKG D112
+
+ 
+
+Monitoring
+
+​
+
+Focal Twin 6 BE Studio Monitors
+
+Yamaha NS10m Studio Monitors
+
+Behritone C5A mono monitor
+
+Beyer dynamic DT770 headphones x2
+
+Beyer Dynamic DT150 headphone
+
+Vic Firth drummer isolation headphones
+
+AKG K701 open back headphones
+
+AKG K77 headphones x2
+
+Tascam MH8 headphone amp
+Palmer Monicon monitor controller
+
+ 
+
+Outboard and I/O
+
+​
+
+Total of 22 channels via....
+RME Fireface 802
+Audient ASP 880
+Focusrite ISA428
+Neve 1073 pre amp x2
+
+Joe Meek VCQ channel strip
+
+CAPI VP 312 pre-amp
+
+Golden Age Project Pre73 pre-amp
+
+TL Audio VP5051 pre-amp
+
+Cranbourne Audio Camden pre-amp
+
+SSL 611 EQ
+
+DBX 560 compressor
+
+      </div>
+  );
+}
+
