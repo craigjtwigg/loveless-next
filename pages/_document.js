7dc82524a1e25e0b58f5c60a44a8c9237e080e4e
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2022-07-10 20:53:36 +0100

    doc file ammended to preload font

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />   
        <link
             href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;500&Fira+Mono:wght@400;700&family=Fira+Sans+Condensed:wght@300;900&display=swap"
             rel="stylesheet"
